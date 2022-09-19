ec145daa0ab62ee0d6184609d52b0ffda709aa29
Alex Elfering <alex.elfering@gmail.com>
2022-09-18 19:24:13 -0500

    updated the location that ggsave points to in the dsm d1b1

--- Des Moines D1B1 Airline Route Map/des moines d1b1 route map.R
+++ Des Moines D1B1 Airline Route Map/des moines d1b1 route map.R
@@ -262,4 +262,4 @@ final_map <- ggplot() +
         panel.grid.major.x = ggplot2::element_blank(),
         panel.grid.major.y = ggplot2::element_blank())
 
-ggsave(final_map, file = '~/des moines route map d1b1.png', width = 36, height = 24, units = c('in'))
+ggsave(final_map, file = '~/GitHub/Airlines/Des Moines D1B1 Airline Route Map/des moines route map d1b1.png', width = 21, height = 12, units = c('in'))
