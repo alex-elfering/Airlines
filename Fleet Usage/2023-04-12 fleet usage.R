3dce9fd45dec6007b8da5f0f6c9f9d33b181146e
Alex <109438131+alex-elfering@users.noreply.github.com>
2023-04-14 09:17:48 -0500

    changed fleet utilize to AA

--- Fleet Usage/2023-04-12 fleet usage.R
+++ Fleet Usage/2023-04-12 fleet usage.R
@@ -24,12 +24,10 @@ colnames(airline_t2) <- tolower(colnames(airline_t2))
 aircraft <- read.csv('C:/Users/alexe/OneDrive/Desktop/L_AIRCRAFT_TYPE.csv')
 colnames(aircraft) <- tolower(colnames(aircraft))
 
-us_airlines <- c('AA', 'UA', 'DL', 'US', 'NW', 'CO', 'TW', 'HA', 'WN', 'NK', 'B6', 'F9', 'G4')
-
 # data prep ----
 
 intl_domestic_seats_fleet <- airline_t2 %>%
-  filter(unique_carrier %in% us_airlines) %>%
+  filter(unique_carrier %in% c('AA')) %>%
   select(year,
          unique_carrier,
          unique_carrier_name,
@@ -90,115 +88,12 @@ intl_domestic_seats_fleet <- airline_t2 %>%
                                    grepl('MD-11', description) ~ 1,
                                  TRUE ~ 0))
 
-# twin aisle asm share in the domestic market ----
-intl_domestic_seats_fleet %>%
-  filter(intl == 0) %>%
-  group_by(unique_carrier,
-           year,
-           twin_aisle) %>%
-  summarise(asm = sum(avl_seat_miles_320)) %>%
-  ungroup() %>%
-  group_by(year,
-           unique_carrier) %>%
-  mutate(pct_asm = asm/sum(asm)) %>%
-  ungroup() %>%
-  filter(twin_aisle == 1) %>%
-  ggplot() + 
-  geom_line(mapping = aes(x = year,
-                          y = pct_asm,
-                          group = unique_carrier),
-            size = 1.8,
-            color = 'white') + 
-  geom_line(mapping = aes(x = year,
-                          y = pct_asm,
-                          color = unique_carrier),
-            size = 1) +
-  theme(
-    plot.title = element_text(face = 'bold', 
-                              size = 14, 
-                              family = 'Noto Sans'),
-    plot.subtitle = element_markdown(),
-    plot.caption = element_text(size = 10,
-                                family = 'Noto Sans',
-                                hjust = 0),
-    axis.title =  ggplot2::element_blank(),
-    axis.text.x = element_text(size = 12, 
-                               #face= bold_label,
-                               family = 'Noto Sans'),
-    axis.text.y = element_text(size = 12, 
-                               family = 'Noto Sans'),
-    strip.text = ggplot2::element_text(size = 12, 
-                                       face = 'bold',
-                                       hjust = 0, 
-                                       family = 'Noto Sans'),
-    plot.title.position = "plot", 
-    plot.caption.position = 'plot',
-    legend.position = 'top',
-    legend.background=element_blank(),
-    legend.key=element_blank(),
-    legend.text = element_text(size = 16),
-    legend.title = element_text(size = 12),
-    axis.line.x.bottom = element_blank(),
-    axis.line.y.left = element_blank(),
-    axis.ticks.y= ggplot2::element_blank(), 
-    axis.ticks.x = ggplot2::element_blank(),
-    strip.background = element_rect(fill = NA),
-    #plot.background = element_rect(fill = "transparent", color = NA),
-    panel.background = ggplot2::element_blank(),
-    panel.grid.minor = element_blank(),
-    panel.grid.major = ggplot2::element_blank()) 
-
-# international asms by airline ----
-intl_domestic_seats_fleet %>%
-  #filter(unique_carrier == 'AA') %>%
-  #filter(grepl('767-3', description)) %>%
-  filter(intl == 1) %>%
-  group_by(year,
-           unique_carrier) %>%
-  summarise(asm = sum(avl_seat_miles_320)) %>%
-  ungroup() %>%
-  group_by(unique_carrier) %>%
-  complete(year = seq(1991, 2022)) %>%
-  ungroup() %>%
-  replace(is.na(.), 0) %>%
-  group_by(year) %>%
-  mutate(pct_asm = asm/sum(asm)) %>%
-  ungroup() %>%
-  ggplot() + 
-  geom_area(mapping = aes(x = year,
-                          y = asm,
-                          fill = unique_carrier))
-
-intl_domestic_seats_fleet %>%
-  #filter(unique_carrier == 'AA') %>%
-  #filter(grepl('767-3', description)) %>%
-  filter(intl == 1) %>%
-  group_by(year,
-           unique_carrier) %>%
-  summarise(asm = sum(avl_seat_miles_320)) %>%
-  ungroup() %>%
-  group_by(unique_carrier) %>%
-  complete(year = seq(1991, 2022)) %>%
-  ungroup() %>%
-  replace(is.na(.), 0) %>%
-  group_by(year) %>%
-  mutate(pct_asm = asm/sum(asm)) %>%
-  ungroup() %>%
-  ggplot() + 
-  geom_area(mapping = aes(x = year,
-                          y = pct_asm,
-                          fill = unique_carrier))
-
-# fleet utilization ----
-
 shorter_plane_names <- intl_domestic_seats_fleet %>%
-  #filter(unique_carrier == 'DL') %>%
-  #filter(intl == 0) %>%
   distinct(description) %>%
   as.data.frame() %>%
   mutate(group = case_when( grepl('737-3', description) | grepl('737-5', description) | grepl('737-1', description) | grepl('737-2', description) | grepl('737-4', description) ~ 'B737 Classic',
                             grepl('737-7', description) | grepl('737-8', description) | grepl('737-9', description) | grepl("B737", description) ~ 'B737 NG/MAX',
-                            grepl('Super 80', description) | grepl('MD-90', description) | grepl('Super 87', description) ~ 'MD-80 series/87/88/90',
+                            grepl('Super 80', description) | grepl('MD-90', description) | grepl('Super 87', description) ~ 'MD-80 series/90',
                             grepl('A300', description) ~ 'A300',
                             grepl('DC-10', description) ~ 'DC-10',
                             grepl('DC-8', description) ~ 'DC-8',
@@ -222,16 +117,7 @@ shorter_plane_names <- intl_domestic_seats_fleet %>%
   )) %>%
   arrange(description)
 
-top_6_fleet <- intl_domestic_seats_fleet %>%
-  filter(unique_carrier == 'AA') %>%
-  filter(intl == 0) %>%
-  left_join(shorter_plane_names) %>%
-  group_by(group,
-           twin_aisle) %>%
-  summarise(asm = sum(avl_seat_miles_320)) %>%
-  ungroup() %>%
-  arrange(desc(asm)) %>%
-  filter(row_number() <= 6)
+# fleet utilization ----
 
 time_series_ua <- intl_domestic_seats_fleet %>%
   filter(unique_carrier == 'AA') %>%
@@ -251,17 +137,22 @@ time_series_ua <- intl_domestic_seats_fleet %>%
   group_by(year) %>%
   mutate(share_asm = asm/sum(asm)) %>%
   ungroup() %>%
-  mutate(group_highlight = ifelse(group %in% top_6_fleet$group, group, 'Other')) %>%
+  mutate(group_highlight = ifelse(group %in% c('B737 NG/MAX', 'A320 Family/neo', 'B777', 'B787', 'MD-80 series/90', 'B757', 'B767', 'B727'), group, 'Other'),
+         group_highlight = factor(group_highlight, levels = c('B737 NG/MAX', 'A320 Family/neo', 'B777', 'B787', 
+                                                              'MD-80 series/90', 'B757', 'B767', 'B727', 'Other'))) %>%
   group_by(year,
            group_highlight) %>%
   summarise(share_asm = sum(share_asm)) %>%
-  ungroup() #%>%
-#filter(share_asm > 0)
+  ungroup() 
 
-time_series_ua %>%
-  filter(year == 2000)
+#458cff
+#91acfb
+#c4cdf6
+#f1f1f1
+#fbc6b4
+#fb9b7b
+#f46d43
 
-time_series_ua = time_series_ua[with(time_series_ua, order(group_highlight, levels(unique(time_series_ua$group_highlight)))),]
 
 time_series_ua %>%
   ggplot(aes(x = year,
@@ -269,10 +160,88 @@ time_series_ua %>%
              group = group_highlight,
              fill = group_highlight)) + 
   geom_area() +
-  scale_fill_manual(values = c('MD-80 series/87/88/90',
-                               'B737 NG/MAX',
-                               'A320 Family/neo',
-                               'B757',
-                               'B767',
-                               'B727'))
+  geom_text_repel(data = time_series_ua %>% filter(year == last(year), share_asm > 0),
+            aes(x = year + 0.5, 
+                y = share_asm, 
+                label = group_highlight,
+                color = group_highlight),
+            position = position_stack(vjust = 0.5),
+            fill = 'white',
+            fontface = 'bold',
+            direction = "y",
+            hjust = -0) + 
+  geom_label_repel(data = time_series_ua %>% filter(year == first(year)+1, share_asm > 0),
+            aes(x = year - 0.5, 
+                y = share_asm, 
+                label = group_highlight,
+                color = group_highlight),
+            fill = 'white',
+            fontface = 'bold',
+            direction = "y",
+            position = position_stack(vjust = 0.5)) + 
+  scale_fill_manual(values = c('B737 NG/MAX' = '#f46d43',
+                               'A320 Family/neo' = '#fb9b7b',
+                               'B777' = '#fcb097',
+                               'B787' = '#f9d1c3',
+                               'MD-80 series/90' = '#f0f0f0',
+                               'B757' = '#d9d9d9',
+                               'B767' = '#bdbdbd',
+                               'B727' = '#969696',
+                               'Other' = 'gray30')) +
+  scale_color_manual(values = c('B737 NG/MAX' = '#f46d43',
+                               'A320 Family/neo' = '#fb9b7b',
+                               'B777' = '#fcb097',
+                               'B787' = '#f9d1c3',
+                               'MD-80 series/90' = 'gray30',
+                               'B757' = '#d9d9d9',
+                               'B767' = '#bdbdbd',
+                               'B727' = '#969696',
+                               'Other' = 'gray30')) +
+  labs(title = "American Airlines Fleet Utilization",
+       fill = '',
+       #subtitle = "<span style = 'font-size:13pt'>The share of outbound seats from Florida airports between <b><span style = 'color:#b35806;'>wide-body</span></b> and <b><span style = 'color:#542788;'>narrow-body</span></b></span> planes 2003-2022.",
+       caption = '\nVisualization by Alex Elfering\nSource: Bureau of Transportation Statistics T2: U.S. Air Carrier TRAFFIC And Capacity Statistics by Aircraft Type') +
+  scale_x_continuous(limits = c(1991,2025)) +
+  scale_y_continuous(labels = c('0', '25', '50', '75', '100%'),
+                     limits = c(0,1.1),
+                     expand = c(0, 0.000001)) +
+  #expand_limits(y = 1.18) + 
+  #geom_hline(yintercept = 0) +
+  #geom_hline(yintercept = 0.5,
+  #           linetype = 'dashed') +
+  guides(fill = guide_legend(nrow = 2)) +
+  theme(
+    plot.title = element_text(face = 'bold', 
+                              size = 14, 
+                              family = 'Noto Sans'),
+    plot.subtitle = element_markdown(),
+    plot.caption = element_text(size = 10,
+                                family = 'Noto Sans',
+                                hjust = 0),
+    axis.title =  ggplot2::element_blank(),
+    axis.text.x = element_text(size = 12, 
+                               #face= bold_label,
+                               family = 'Noto Sans'),
+    axis.text.y = element_text(size = 12, 
+                               family = 'Noto Sans'),
+    strip.text = ggplot2::element_text(size = 12, 
+                                       face = 'bold',
+                                       hjust = 0, 
+                                       family = 'Noto Sans'),
+    plot.title.position = "plot", 
+    plot.caption.position = 'plot',
+    legend.position = 'none',
+    legend.background=element_blank(),
+    legend.key=element_blank(),
+    legend.text = element_text(size = 16),
+    legend.title = element_text(size = 12),
+    axis.line.x.bottom = element_blank(),
+    axis.line.y.left = element_blank(),
+    axis.ticks.y= ggplot2::element_blank(), 
+    axis.ticks.x = ggplot2::element_blank(),
+    strip.background = element_rect(fill = NA),
+    #plot.background = element_rect(fill = "transparent", color = NA),
+    panel.background = ggplot2::element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.grid.major = ggplot2::element_blank()) 
 
