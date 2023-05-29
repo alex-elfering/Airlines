11220975908daae1392d1439ce652ad92e0f9cc0
Alex <109438131+alex-elfering@users.noreply.github.com>
2023-05-29 15:08:20 -0500

    Update 2023-05-28 Historic OMA.R

--- Historic OMA Service/2023-05-28 Historic OMA.R
+++ Historic OMA Service/2023-05-28 Historic OMA.R
@@ -12,6 +12,7 @@ library(ggpattern)
 library(gghighlight)
 library(gt)
 library(zoo)
+library(glue)
 
 options("device" = "windows")
 options(scipen = 999)
@@ -57,7 +58,7 @@ outbound_flight_details <- intl_df |>
   mutate(seats_departure = floor(seats/departures_performed),
          air_time_departure = round(air_time/departures_performed),
          ramp_to_ramp_dep = round(ramp_to_ramp/departures_performed),
-         load_factor = round((passengers/seats)*100,1),
+         load_factor = round((passengers/seats)*80,1),
          flight_month = ymd(paste(year, month, '1', '-'))) |>
   filter(seats > 0,
          passengers > 0,
@@ -66,7 +67,7 @@ outbound_flight_details <- intl_df |>
   select(-year,
          -month)
 
-year_var <- 1990
+year_var <- 1995
 
 aircraft_used <- outbound_flight_details |>
   filter(year(flight_month) == year_var) |>
@@ -100,6 +101,12 @@ stats_overall <- outbound_flight_details |>
             mean_tot_departures = mean(departures_performed)) |>
   ungroup()
 
+
+# https://malco.io/2020/05/16/replicating-an-nyt-table-of-swedish-covid-deaths-with-gt/
+plus_percent <- function(.x) {
+  glue::glue("{.x}%")
+}
+
 outbound_flight_details |>
   filter(year(flight_month) == year_var) |>
   #filter(dest == 'LNK') |>
@@ -107,24 +114,37 @@ outbound_flight_details |>
            dest_city_name,
            carrier,
            carrier_name) |>
-  summarise(from_date = as.yearmon(min(flight_month)),
-            to_date = as.yearmon(max(flight_month)),
+  summarise(from_date = month.abb[month(min(flight_month))],
+            to_date = month.abb[month(max(flight_month))],
             mean_seats = mean(seats_departure),
-            mean_ld_fctr = mean(load_factor),
+            mean_ld_fctr = round(mean(load_factor),1),
             mean_air_time = mean(air_time_departure)) |>
   ungroup() |>
   inner_join(stats_overall) |>
   inner_join(aircraft_used) |>
   arrange(desc(mean_tot_passengers)) |>
   unite(date_range, c('from_date', 'to_date'), sep = '-')  |>
+  unite(city_airport, c('dest', 'dest_city_name'), sep = ' ') |>
   mutate(carrier_name = case_when(grepl(' Inc.', carrier_name) ~ gsub(' Inc.', '', carrier_name),
                                   grepl(' Corporation', carrier_name) ~ gsub(' Corporation', '', carrier_name),
                                   grepl(' Co.', carrier_name) ~ gsub(' Co.', '', carrier_name),
                                   TRUE ~ carrier_name)) |>
   select(-carrier) |>
+  select(city_airport,
+         carrier_name,
+         date_range,
+         mean_tot_seats,
+         mean_tot_passengers,
+         mean_tot_departures,
+         mean_seats,
+         mean_ld_fctr,
+         mean_air_time,
+         aircraft_used) |>
+  filter(carrier_name != 'Casino Express') %>%
   gt() |>
   fmt_integer(columns = c('mean_seats', 'mean_ld_fctr', 'mean_air_time', 'mean_tot_seats', 'mean_tot_passengers', 'mean_tot_departures'),
               use_seps = TRUE) |>
+  fmt(mean_ld_fctr, fns = plus_percent) |>
   tab_spanner(
     label = "Per Departure",
     columns = c(mean_seats, mean_ld_fctr, mean_air_time)
@@ -135,7 +155,7 @@ outbound_flight_details |>
   )|>
   tab_style(
     style = cell_text(
-      size = px(12),
+      size = px(13),
       color = "black",
       font = "arial",
       weight = 'bold',
@@ -144,14 +164,14 @@ outbound_flight_details |>
   ) |>
   tab_style(
     style = cell_text(
-      size = px(12),
+      size = px(13),
       color = "#999",
-      font = "franklin",
+      font = "arial",
     ),
     locations = cells_body(columns = c(date_range))
   ) |>
   cols_label(
-    dest = "Destination",
+    city_airport = "Destination",
     carrier_name = "Airline",
     date_range = "Served Between",
     mean_seats = 'Seats',
@@ -161,11 +181,47 @@ outbound_flight_details |>
     mean_tot_passengers = 'Passengers',
     mean_tot_departures = 'Departures',
     aircraft_used = 'Aircraft Used') |>
-  cols_width(dest ~ px(100),
-             carrier_name ~ px(150)) |>
-  tab_options(table.font.size = 12,
-              table.font.names = 'Courier') |>
+  tab_options(table.font.size = 13,
+              table.font.names = 'arial') |>
   tab_header(
-    title = md("Omaha Eppley Airfield Passenger Stats"),
-    subtitle = md("Outbound passenger stats, 1990")
+    title = md(glue("**{year_var} Omaha Eppley Airfield Passenger Stats**")),
+    subtitle = md(glue("Outbound passenger stats by airline and destination YE {year_var}, sorted by passengers per month"))
+  ) |>
+  tab_style(
+    style = cell_borders(
+      sides = c("left", 'right'),
+      color = "white",
+      weight = px(0.5),
+      style = "solid"
+    ),
+    locations = cells_body(columns = everything())
+  ) %>%
+  tab_style(
+    style = cell_borders(
+      sides = c("left"),
+      color = "gray80",
+      weight = px(2),
+      style = "solid"
+    ),
+    locations = cells_body(columns = c(mean_tot_seats, mean_seats, aircraft_used))
+  ) %>%
+  data_color(
+    columns = vars(mean_tot_seats, mean_tot_passengers, mean_tot_departures),
+    colors = scales::col_numeric(
+      # custom defined values - notice that order matters!
+      palette = (c('#f1eef6', '#d0d1e6', '#a6bddb', '#74a9cf', '#2b8cbe', '#045a8d')),
+      domain = NULL
+    )
+  )  %>%
+  data_color(
+    columns = vars(mean_seats,mean_air_time,mean_ld_fctr),
+    colors = scales::col_numeric(
+      # custom defined values - notice that order matters!
+      palette = (c('#fef0d9', '#fdd49e', '#fdbb84', '#fc8d59', '#e34a33', '#b30000')),
+      domain = NULL
+    )
+  ) |>
+  cols_align(
+    align = c("center"),
+    columns = c(mean_seats, mean_ld_fctr, mean_air_time, mean_tot_seats, mean_tot_passengers, mean_tot_departures)
   )
