8130c11607b2abeae89ee44cb9517d8cd2a855ec
Alex <109438131+alex-elfering@users.noreply.github.com>
2024-02-23 17:59:00 -0600

    added United Airlines script

--- United Airlines Hubs/2024-02-23 United Airlines Hubs.R
+++ United Airlines Hubs/2024-02-23 United Airlines Hubs.R
@@ -16,16 +16,42 @@ setwd("C:/Users/alexe/OneDrive/Desktop/intl seg")
 t_100_intl <- list.files("C:/Users/alexe/OneDrive/Desktop/intl seg", pattern = "*.csv")
 t_100_intl_df <- rbindlist(lapply(t_100_intl, fread)) |> clean_names()
 
+setwd("C:/Users/alexe/OneDrive/Desktop/t-100 dom")
+t_100_dom <- list.files("C:/Users/alexe/OneDrive/Desktop/t-100 dom", pattern = "*.csv")
+t_100_dom_df <- rbindlist(lapply(t_100_intl, fread)) |> clean_names()
+
 aircraft <- read.csv('C:/Users/alexe/OneDrive/Desktop/L_AIRCRAFT_TYPE.csv') |> clean_names()
 
 # filtering and cleaning  ----
 
-#airline_var <- c('F9', 'AA','UA','DL','CO','TW','NW','B6','WN','G4', 'NK','HA','AS','VX', 'US', 'HP')
+airline_var <- c('F9', 'AA','UA','DL','CO','TW','NW','B6','WN','G4', 'NK','HA','AS','VX', 'US', 'HP')
 #hub_var <- c('ATL', 'MDW', 'ORD', 'CVG', 'CLE', 'DFW', 'DEN', 'LAS', 'MIA', 'MCO', 'PHL', 'PHX', 'SJU', 'TPA', 'MKE', 'MCI')
 
+domestic_sum <- t_100_dom_df %>%
+  filter(carrier %in% c('UA')) |>
+  filter(departures_performed >= 10) %>%
+  mutate(dest_country = 'US',
+         origin_country = 'US') |>
+  group_by(unique_carrier_name,
+           origin_country,
+           origin_city_name,
+           origin,
+           dest_city_name,
+           dest_country,
+           dest,
+           year,
+           month,
+           aircraft_type,
+           carrier) %>%
+  summarise(seats = sum(seats),
+            passengers = sum(passengers),
+            departures = sum(departures_performed),
+            months = n_distinct(month)) %>%
+  ungroup() |>
+  as.data.frame()
+
 international_sum <- t_100_intl_df %>%
-  filter(carrier %in% c('DL'),
-         origin == 'JFK') |>
+  filter(carrier %in% c('UA')) |>
   filter(departures_performed >= 10) %>%
   group_by(unique_carrier_name,
            origin_country,
@@ -45,7 +71,7 @@ international_sum <- t_100_intl_df %>%
   ungroup() |>
   as.data.frame()
 
-international_stats <- international_sum  %>%
+international_stats <- bind_rows(international_sum, domestic_sum)  %>%
   inner_join(aircraft, by = c('aircraft_type' = 'code')) |>
   arrange(year) |>
   as.data.frame() |>
@@ -55,21 +81,23 @@ international_stats <- international_sum  %>%
   arrange(desc(load_factor)) |>
   mutate(international_service = ifelse(dest_country == 'US', 0, 1))
 
-delta_dest_history <- international_stats |>
+united_dest_history <- international_stats |>
   group_by(year,
            #carrier,
            origin,
+           origin_city_name,
            dest,
            dest_city_name,
            international_service) |>
   summarise(seats = sum(seats)) |>
   ungroup()
 
-origin_dest_service_history <- delta_dest_history |>
+origin_dest_service_history <- united_dest_history |>
   group_by(#carrier,
-           origin,
-           dest,
-           dest_city_name) |>
+    origin,
+    origin_city_name,
+    dest,
+    dest_city_name) |>
   complete(year = seq.int(min(year), 2023, by = 1)) |>
   mutate(service = ifelse(!is.na(seats), 1, 0),
          service_ends = ifelse(lead(is.na(seats)) & !is.na(seats), 1, 0),
@@ -88,7 +116,14 @@ origin_dest_service_history <- delta_dest_history |>
                                    service_ends == 1 & service_resumes == 1 ~ 'Service Resumed, then Terminated',
                                    service_ends == 0 & service_resumes == 1 ~ 'Service Resumed',
                                    TRUE ~ 'Service')) |>
-  #filter(service_level != 'Service') |>
-  mutate(service_level = factor(service_level, levels = c('Service Established', 'Service Resumed', 'Service', 'Service Established, then Terminated', 'Service Resumed, then Terminated', 'Service Terminated')))
+  mutate(service_level = case_when(service_level == 'Service Established' & year == 1990 ~ 'Service',
+                                   TRUE ~ service_level)) |>
+  mutate(service_level = factor(service_level, levels = c('Service Established', 'Service Resumed', 'Service', 'Service Established, then Terminated', 'Service Resumed, then Terminated', 'Service Terminated'))) |>
+  separate(dest_city_name, into = c('city', 'city1'), sep = ', ') |>
+  separate(origin_city_name, into = c('origin_city', 'city2'), sep = ', ') |>
+  mutate(state = ifelse(international_service == 0, city1, ''),
+         country = ifelse(international_service == 1, city1, 'United States')) |>
+  select(-city1,
+         -city2)
 
-write.csv(origin_dest_service_history,"C:/Users/alexe/OneDrive/Documents/GitHub/Airlines/Delta Air Lines JFK Hub/origin_dest_service_history.csv")
+write.csv(origin_dest_service_history,"C:/Users/alexe/OneDrive/Documents/GitHub/Airlines/Historic Airline Hub Service/origin_dest_service_history.csv")
