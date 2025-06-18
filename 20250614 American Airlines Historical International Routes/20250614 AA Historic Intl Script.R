3b44de92d687a5dc37ed208c558014846eb5b259
Alex <109438131+alex-elfering@users.noreply.github.com>
2025-06-18 14:50:04 -0500

    removed the text package references

--- 20250614 American Airlines Historical International Routes/20250614 AA Historic Intl Script.R
+++ 20250614 American Airlines Historical International Routes/20250614 AA Historic Intl Script.R
@@ -2,21 +2,13 @@
 
 #source('~/GitHub/Airlines/2025 Route History Analysis/20250510 Route History.R')
 
-aiports_df <- airportr::airports
-
 library(sf)
 library(rnaturalearth)
 library(tidyverse)
 library(ggrepel)
-#install.packages("extrafont")
-library(extrafont)
-font_import()  # This takes a while — one time only
-loadfonts(device = "win")  # For Windows
-#install.packages("showtext")
-library(showtext)
 library(janitor)
-showtext_auto()
-font_add("Arial", "arial.ttf")
+
+set.seed(123)
 
 airline_var <- 'AA'
 
@@ -25,10 +17,10 @@ flights_90s <- history |>
   filter(carrier %in% c(airline_var),
          year <= 2000,
          international_service == 1,
-         seats > 0) %>%
-  left_join(airportr::airports, by = c("origin" = "IATA")) %>%
-  rename(origin_lat = Latitude, origin_lon = Longitude) %>%
-  left_join(airportr::airports, by = c("dest" = "IATA")) %>%
+         seats > 0) |>
+  left_join(airportr::airports, by = c("origin" = "IATA")) |>
+  rename(origin_lat = Latitude, origin_lon = Longitude) |>
+  left_join(airportr::airports, by = c("dest" = "IATA")) |>
   rename(dest_lat = Latitude, dest_lon = Longitude) |>
   clean_names() |>
   filter(grepl('Asia',timezone_y) | grepl('Europe',timezone_y) | grepl('Africa',timezone_y)) |>
@@ -40,45 +32,82 @@ flights_90s <- history |>
          dest_lat,
          dest_lon) |>
   distinct() |>
-  filter(!is.na(origin_lon), !is.na(origin_lat), !is.na(dest_lon), !is.na(dest_lat))
+  dplyr::filter(!is.na(origin_lon), !is.na(origin_lat), !is.na(dest_lon), !is.na(dest_lat))
 
 flights_23 <- history |>
   filter(carrier %in% c(airline_var),
          year == 2023,
          international_service == 1,
-         seats > 0) %>%
-  left_join(airportr::airports, by = c("origin" = "IATA")) %>%
-  rename(origin_lat = Latitude, origin_lon = Longitude) %>%
-  left_join(airportr::airports, by = c("dest" = "IATA")) %>%
+         seats > 0) |>
+  left_join(airportr::airports, by = c("origin" = "IATA")) |>
+  rename(origin_lat = Latitude, origin_lon = Longitude) |>
+  left_join(airportr::airports, by = c("dest" = "IATA")) |>
   rename(dest_lat = Latitude, dest_lon = Longitude) |>
   clean_names() |>
   filter((grepl('Asia',timezone_y) | grepl('Europe',timezone_y))) |>
   select(carrier,
          origin,
-         dest,
-         origin_lat,
-         origin_lon,
-         dest_lat,
-         dest_lon) |>
-  distinct() |>
-  filter(!is.na(origin_lon), !is.na(origin_lat), !is.na(dest_lon), !is.na(dest_lat))
+         dest) |>
+  distinct()
 
 flights_from_90s <- flights_90s |>
   anti_join(flights_23)
+
+history |>
+  filter(carrier %in% c(airline_var),
+         #year <= 2000,
+         international_service == 1,
+         seats > 0) |>
+  left_join(airportr::airports, by = c("origin" = "IATA")) |>
+  rename(origin_lat = Latitude, origin_lon = Longitude) |>
+  left_join(airportr::airports, by = c("dest" = "IATA")) |>
+  rename(dest_lat = Latitude, dest_lon = Longitude) |>
+  clean_names() |>
+  filter(grepl('Asia',timezone_y) | grepl('Europe',timezone_y)) |>
+  anti_join(flights_23) |>
+  inner_join(flights_90s) |>
+  select(carrier,
+         origin,
+         dest,
+         year) |>
+  arrange(carrier, origin, dest, year) %>%
+  group_by(carrier, origin, dest) %>%
+  mutate(
+    year = as.integer(year),
+    gap = year - lag(year, default = first(year)),
+    grp = cumsum(gap != 1)
+  )%>%
+  group_by(carrier, origin, dest, grp) %>%
+  summarise(
+    start = min(year),
+    end = max(year),
+    .groups = "drop"
+  )%>%
+  mutate(
+    streak = ifelse(start == end, as.character(start), paste0(start, "-", end))
+  ) %>%
+  group_by(carrier, origin, dest) %>%
+  summarise(service_streaks = paste(streak, collapse = ", "),
+            last_year = max(end),
+            .groups = "drop"
+  ) %>%
+  arrange(desc(last_year),origin) |>
+  as.data.frame()
+  
 
 # Define LAEA projection centered on the Americas
 laea_proj <- "+proj=laea +lat_0=55 +lon_0=30 +datum=WGS84 +units=m +no_defs"
 
 # Load and reproject world data
-world <- ne_countries(scale = "medium", returnclass = "sf") %>%
+world <- ne_countries(scale = "medium", returnclass = "sf") |>
   st_transform(laea_proj)
 
 # Bounding box in projected coordinates (meters)
 bbox <- st_bbox(c(
-  xmin = -7300000,   # Covers western North America
-  xmax = 7500000,   # Extends east to cover all of Asia
-  ymin = -1000000,   # Still cuts off below Central America
-  ymax = 9000000     # Slightly higher to give more Arctic space
+  xmin = -7300000,   
+  xmax = 7500000,   
+  ymin = -1000000,   
+  ymax = 9000000     
 ), crs = laea_proj)
 world_crop <- st_crop(world, bbox)
 
@@ -104,38 +133,34 @@ make_arch_curve <- function(p1, p2, arc_height = 0.3, direction = 1, n = 100) {
   return(bez_coords)
 }
 
-#direction <- ifelse(dest_lon - origin_lon > 0, 1, -1)
-
-routes <- flights_from_90s %>%
-  select(origin, dest, origin_lon, origin_lat, dest_lon, dest_lat) %>%
+routes <- flights_from_90s |>
+  select(origin, dest, origin_lon, origin_lat, dest_lon, dest_lat) |>
   pmap_dfr(function(origin, dest, origin_lon, origin_lat, dest_lon, dest_lat) {
-    p1 <- st_transform(st_sfc(st_point(c(origin_lon, origin_lat)), crs = 4326), laea_proj) %>% st_coordinates()
-    p2 <- st_transform(st_sfc(st_point(c(dest_lon, dest_lat)), crs = 4326), laea_proj) %>% st_coordinates()
+    p1 <- st_transform(st_sfc(st_point(c(origin_lon, origin_lat)), crs = 4326), laea_proj) |> st_coordinates()
+    p2 <- st_transform(st_sfc(st_point(c(dest_lon, dest_lat)), crs = 4326), laea_proj) |> st_coordinates()
     
     # Flip up/down based on longitudes
     direction <- sample(c(-1, 1), 1)
     
-    coords <- make_arch_curve(p1, p2, arc_height = 0.4, direction = direction)
+    coords <- make_arch_curve(p1, p2, arc_height = 0.43, direction = direction)
     
     st_linestring(coords) |>
       st_sfc(crs = laea_proj) |>
       st_sf(route = paste(origin, "to", dest), geometry = _)
   })
 
-city_points <- flights_from_90s %>%
-  select(city = origin, lon = origin_lon, lat = origin_lat) %>%
+city_points <- flights_from_90s |>
+  select(city = origin, lon = origin_lon, lat = origin_lat) |>
   bind_rows(
-    flights_from_90s %>% select(city = dest, lon = dest_lon, lat = dest_lat)
-  ) %>%
-  distinct() #%>%
-#mutate(is_hub = city %in% hub_airports)
+    flights_from_90s |> select(city = dest, lon = dest_lon, lat = dest_lat)
+  ) |>
+  distinct() 
 
 # Convert to sf points
-cities_sf <- st_as_sf(city_points, coords = c("lon", "lat"), crs = 4326) %>%
+cities_sf <- st_as_sf(city_points, coords = c("lon", "lat"), crs = 4326) |>
   st_transform(laea_proj)
 
-cities_label_df <- cities_sf %>%
-  #filter(!city %in% c("DTW", "MSP", "NRT", "MEM", "AMS")) %>%
+cities_label_df <- cities_sf |>
   mutate(
     coords = st_coordinates(geometry),
     x = coords[,1],
@@ -144,7 +169,6 @@ cities_label_df <- cities_sf %>%
   left_join(airportr::airports,
             by = c('city' = 'IATA')) |>
   select(city,
-         #is_hub,
          geometry,
          coords,
          x,
@@ -169,17 +193,19 @@ cities_label_df <- cities_sf %>%
                           city == 'PVG' ~ 'Shanghai-Pudong',
                           city == 'FCO' ~ 'Rome-Fiumicino',
                           city == 'RDU' ~ 'Raleigh-Durdam',
+                          city == 'NGO' ~ 'Nagoya-Centrair',
                           TRUE ~ City))
 
 ggplot() +
   geom_sf(data = world_crop, fill = "gray85", color = "white", size = 12) +
   geom_sf(data = routes, size = 0.005, alpha = 0.2, color = '#bd0026') +
   geom_sf(data = cities_sf, color = '#bd0026', size = 0.7) +
+  geom_sf(data = cities_sf, color = 'black', size = 0.7,shape = 1) +
   coord_sf(crs = laea_proj, expand = FALSE) +
   geom_text_repel(
     data = cities_label_df,
     aes(x = x, y = y, label = City),
-    size = 8,
+    size = 2,
     fontface = "bold",
     box.padding = 0.3,
     point.padding = 0.1,
@@ -187,15 +213,20 @@ ggplot() +
     segment.color = "black",
     segment.size = 0.1
   ) +
-  labs(title = 'International Routes Flown by American Throughout the 90s',
-       caption = 'Code by Alex Elfering | Source: Bureau of Transportation Statistics') +
+  labs(title = "Discontinued European and Asian Routes by American Airlines",
+       subtitle = 'International nonstop routes flown throughout the 1990s but absent by 2023',
+       caption = 'Code by Alex Elfering | Source: Bureau of Transportation Statistics T-100 International Segment Data') +
   theme_void() +
   theme(
     panel.background = element_rect(fill = "white", color = NA),
     plot.background = element_rect(fill = "white", color = NA),
     legend.position = 'top',
-    plot.title = element_text(face = 'bold',family = 'Arial', size = 24),
-    plot.subtitle = element_text(face = 'bold',size = 20,family = 'Arial')
+    plot.title = element_text(face = 'bold', 
+                              size = 18),
+    plot.caption = element_text(size = 8,
+                                family = 'Noto Sans',
+                                hjust = 0,
+                                color = 'gray50'),
   ) 
 
-ggsave(file = ('C:/Users/alexe/OneDrive/Desktop/NW route map.png'), width = 10, height = 7, dpi = 350, units = "in")
+ggsave(file = ('~/GitHub/Airlines/20250614 American Airlines Historical International Routes/historic route map.png'), width = 10, height = 7, dpi = 350, units = "in")
